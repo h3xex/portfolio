ba7eb7bcb90e1d7bb07adb1c6236d4c320829891
h3xex <62241425+h3xex@users.noreply.github.com>
2022-06-07 19:01:48 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const aboutMeTitle = document.getElementById("aboutMeTitle");
+const aboutMeBio = document.getElementById("aboutMeBio");
+
+const titleProgrammer = "I'm a programmer";
+const titleStudent = "I'm a student";
+const titleGamer = "I'm a gamer";
+
+const typewriteSpeed = 100;
